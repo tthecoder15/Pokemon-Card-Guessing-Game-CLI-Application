114f623fc6b663f320b67c1d82c885b663cf600c
tthecoder15 <ttutone15@gmail.com>
2024-05-07 15:39:32 +1000

    Fixed formatting of prior hints printing to console, fixed error when type hint was requested

--- src/game_dialogue.py
+++ src/game_dialogue.py
@@ -8,18 +8,20 @@ standard_dialogue = {
     "hint_1": "The Pokémon's description reads: ",
     "guess_or_hint1": "Are you ready to guess or would you like another hint? To guess type 'guess' and press enter. For another hint type 'hint' and press enter. ",
     "guess_or_hint2": "Surely you've got it now! Are you ready to guess or would you like another hint? To guess type 'guess' and press enter. For YET ANOTHER hint type 'hint' and press enter. ",
-    "guess_time": "I can't offer anymore hints or it'd be too easy! It's time to guess!",
+    "guess_time": "Now, I can't offer anymore hints or it'd be too easy! It's time to guess!",
+
+    "guess_prompt": "Enter the name of the mysterious Pokémon: ",
+    "answer_correct": "You are...CORRECT! Professor Oak would be proud!",
+    "answer_incorrect": "You are...unfortunately, incorrect. Nice try though.",
+    'running_score' : "Your session score is: ",
+    'record_score' : "Hey! Your score is good enough to go on the leaderboard!",
+    "play_again" : "Would you like to play again?",
+
     "2nd_hint_prompt": "For your second hint, do you want to know the Pokémon's retreat cost or one of their attacks? Remember, these attributes change between different sets! Type 'retreat cost' or 'attack' then press enter: ",
     "2nd_hint_retreat": "The Pokemon's retreat cost, on this card, is: ",
     "2nd_hint_attack": "One of their attacks is called: ",
     "3rd_hint_prompt": "For your final hint, would you like to know the Pokémon's evolution stage or their type? For evolution stage write 'evolution' and press the enter key. For type write 'type' and press the enter key: ",
     "3rd_hint_stage": "This Pokémon is a: ",
     "3rd_hint_type": "This Pokémon's type is: ",
-    "summary": "Here are the hints you've had so far: ",
-    "guess_prompt": "Enter the name of the mysterious Pokémon: ",
-    "answer_correct": "You are...CORRECT! Professor Oak would be proud!",
-    "answer_incorrect": "You are...unfortunately, incorrect. Nice try though.",
-    'running_score' : "Your session score is: ",
-    'record_score' : "Hey! Your score is good enough to go on the leaderboard!",
-    "play_again" : "Would you like to play again?"
+    "summary": "Here are the hints you've had so far: "
 }
--- src/main.py
+++ src/main.py
@@ -43,7 +43,8 @@ def gen_rand_card():
         'set_number' : rand_card["number"],
         'flavor_text' : rand_card["flavorText"],
         'ret_cost' : str(rand_card["convertedRetreatCost"]),
-        'atks' : rand_card["attacks"]}
+        'atks' : rand_card["attacks"],
+        'type' : rand_card['types']}
 
 
 # execution
@@ -70,30 +71,49 @@ def gen_rand_card():
 
 round_hints = {}
 
+def guess():
+    print("This will trigger the guessing component of the game")
+    raise Exception("Guessing function not yet defined")
+
+def hard_mode_game():
+    print("This will trigger the hard mode of the game")
+    raise Exception("Hard mode function not yet defined")
+
+def scoreboard():
+    print("This will trigger the scoreboard functionality")
+    raise Exception("Scoreboard functionality not defined yet")
+
 def update_print_round_hints(hint_label, dialogue, hint):
     key_value = dialogue + '\n' + '"' + hint + '"' + '\n'
     round_hints.update({hint_label : key_value})
     return print(round_hints[hint_label])
 
 def hint_reminder(*hints_received):
+    print(standard_dialogue['summary'] + '\n')
     for hint in hints_received:
-        print("Remember, these are the hints you've already received: " + '\n' + round_hints[hint])
+        print(round_hints[hint])
 
 def standard_game():
-    while True: 
-        if input(menu['greeting']).lower() == "scoreboard":
+    os.system('clear')
+
+    print(menu['greeting'])
+    while True:
+        response = input()
+        if response.lower() == "scoreboard" or response.lower() == "'scoreboard'":
         # Need to return scoreboard here
-            print("THIS WILL SHOW THE SCOREBOARD")
-            raise Exception("SCOREBOARD HERE")
+            scoreboard()
     
         else:
             break
-                
+
+    os.system('clear')        
+    print(menu['mode_choice'])            
+    
     while True:
-        if input(menu['mode_choice']).lower() == "hard":
-            # Need to run hardmode here
-            print("THIS WILL RUN HARDMODE")
-            raise Exception("HARD MODE HERE")
+        response = input()
+        if response.lower() == "hard" or response.lower() == "'hard'":
+        # Need to run hardmode here
+            hard_mode_game()
         
         else:
             break
@@ -102,63 +122,71 @@ def standard_game():
     round_card = gen_rand_card()
     update_print_round_hints('hint_1', standard_dialogue['hint_1'], round_card['flavor_text'])
     
-    while True:
-        response = input(standard_dialogue['guess_or_hint1'])
+    print(standard_dialogue['guess_or_hint1'])
+    while True:       
+        response = input()
         if response.lower() == 'guess' or response.lower() == "'guess'":
-            print("THIS WILL BEGIN THE GUESSING COMPONENT")
-            raise Exception("GUESSING FUNCTION HERE")
+            guess()
         
         elif response.lower() == 'hint' or response.lower() == "'hint'":
             break
 
         else:
-            print("Please type 'guess' or 'hint'." + "\n")
-        
+            print("\n" + "Please type 'guess' or 'hint'." + "\n")    
     os.system('clear')
+    
+    print(standard_dialogue['2nd_hint_prompt'])
     while True:
-        response = input(standard_dialogue['2nd_hint_prompt'])
+        response = input()
         
         if response.lower() == 'retreat cost' or response.lower() == "'retreat cost'":         
+            os.system('clear')
             update_print_round_hints('hint_2', standard_dialogue['2nd_hint_retreat'], round_card['ret_cost'])
             break
 
         if response.lower() == 'attack' or response.lower() == "'attack'":         
+            os.system('clear')
             update_print_round_hints('hint_2', standard_dialogue['2nd_hint_attack'], round_card['atks'][random.randint(0, len(round_card['atks']))-1]['name'])
             break
 
         else:
-            print("Please type 'retreat cost' or 'attack'." + "\n")
-    hint_reminder('hint_1')
+            print("\n" + "Please type 'retreat cost' or 'attack'." + "\n")
 
+
+    hint_reminder('hint_1')
+    print(standard_dialogue['guess_or_hint2'])
     while True:
-        response = input(standard_dialogue['guess_or_hint2'])
+        response = input()
         if response.lower() == 'guess' or response.lower() == "'guess'":
-            print("THIS WILL BEGIN THE GUESSING COMPONENT")
-            raise Exception("GUESSING FUNCTION HERE")
+            guess()
         
         elif response.lower() == 'hint' or response.lower() == "'hint'":
             break
 
         else:
-            print("Please type 'guess' or 'hint'." + "\n")
-        
+            print("\n" + "Please type 'guess' or 'hint'." + "\n")
     os.system('clear')
+
+    print(standard_dialogue['3rd_hint_prompt'])
     while True:
-        response = input(standard_dialogue['3rd_hint_prompt'])
+        response = input()
         
         if response.lower() == 'evolution' or response.lower() == "'evolution'":         
+            os.system('clear')
             update_print_round_hints('hint_3', standard_dialogue['3rd_hint_stage'], round_card['stage'])
             break
 
         if response.lower() == 'type' or response.lower() == "'type'":         
-            update_print_round_hints('hint_3', standard_dialogue['3rd_hint_type'], round_card['type'])
+            os.system('clear')
+            update_print_round_hints('hint_3', standard_dialogue['3rd_hint_type'], round_card['type'][0])
             break
 
         else:
-            print("Please type 'retreat cost' or 'attack'." + "\n")
+            print("\n" + "Please type 'retreat cost' or 'attack'." + "\n")
 
     hint_reminder('hint_1', 'hint_2')
-    print("This is where we're at yo!!!!")
+    print(standard_dialogue['guess_time'])
+    guess()
 
 # print(gen_rand_card())
 standard_game()
